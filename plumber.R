df384e57aaab4bcb95d69307860263dd5e23d5c3
David Newton <david.newton@nist.gov>
2025-07-21 00:03:38 -0600

    implement plumber api

--- plumber.R
+++ plumber.R
@@ -1,12 +1,3 @@
-#
-# This is a Plumber API. You can run the API by clicking
-# the 'Run API' button above.
-#
-# Find out more about building APIs with Plumber here:
-#
-#    https://www.rplumber.io/
-#
-
 library(plumber)
 source('R/utils.R')
 source('R/KCplotDoEplot_6_22.R')
@@ -14,8 +5,23 @@ source('R/sampleFromTau2Dist.R')
 source('R/symmetricalBootstrapCI.R')
 source('R/weightedMedian.R')
 
+# default parameters
+default_params = list(
+  laboratory=c("Lab1","Lab2","Lab3","Lab4","Lab5"),
+  measured_values=c(1,3,5,4,2),
+  std_unc=c(1,2,1,2,1),
+  dof=rep(30,5),
+  exclude=rep(FALSE,5),
+  procedure="Recommended",
+  num_bootstrap=1000,
+  seed=123,
+  n_iter=50000,
+  burn_in=25000
+)
+
+
 #* @apiTitle NDT API
-#* @apiDescription Plumber example description.
+#* @apiDescription Implements NDT as a REST API
 
 #* Echo back the input
 #* @get /test
@@ -23,51 +29,35 @@ function(res) {
   return("Server is up and running.")
 }
 
-#* Test post
-#* @param msg
-#* @post /test
-function(msg) {
-  return(msg)
-}
-
-
 #* Run selected NDT procedure
-#* @param laboratory:[str]
-#* @param measured_values:[double] Measurement results
-#* @param std_unc:[double] Measurement uncertainties
-#* @param dof:[int] Degrees of freedom
-#* @param exclude:[bool] Vector of labs to be excluded (optional)
-#* @param procedure Selected procedure
-#* @param num_bootstrap:int Number of bootstrap samples for DSL
-#* @param seed:int Random number seed
-#* @param n_iter:int Number Jags Iterations
-#* @param burn_in:int Number of burn-in samples
-#* @post /run_ndt_query_params
-function(req,res,
-         laboratory=c("Lab1","Lab2","Lab3","Lab4","Lab5"),
-         measured_values=c(1,3,5,4,2),
-         std_unc=c(1,2,1,2,1),
-         dof=rep(30,5),
-         exclude=rep(FALSE,5),
-         procedure="Recommended",
-         num_bootstrap=1000,
-         seed=123,
-         n_iter=50000,
-         burn_in=25000) {
+#* @param req
+#* @post /run_ndt
+function(req) {
+  
+  # get post body
+  body <- jsonlite::fromJSON(req$postBody)
+
+  # fill in body arguments with defaults where missing
+  for(n in names(default_params)) {
+    if(is.null(body[[n]])) {
+      body[[n]] = default_params[[n]]
+    }
+  }
   
   dataset = data.frame(
-    Laboratory = laboratory,
-    MeasuredValues = as.numeric(measured_values),
-    StdUnc = as.numeric(std_unc),
-    DegreesOfFreedom = as.numeric(dof)
+    Laboratory = body$laboratory,
+    MeasuredValues = as.numeric(body$measured_values),
+    StdUnc = as.numeric(body$std_unc),
+    DegreesOfFreedom = as.numeric(body$dof)
   )
   
-  exclude = exclude == 'true'
-  num_bootstrap = as.numeric(num_bootstrap)
-  seed = as.numeric(seed)
-  n_iter = as.numeric(n_iter)
-  burn_in = as.numeric(burn_in)
+  exclude = body$exclude == 'true'
+  num_bootstrap = as.numeric(body$num_bootstrap)
+  seed = as.numeric(body$seed)
+  n_iter = as.numeric(body$n_iter)
+  burn_in = as.numeric(body$burn_in)
   
+  # run ndt
   res = run_full_ndt(dataset,
                      exclude,
                      procedure = "Recommended",
